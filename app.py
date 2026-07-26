c4f890609be9b323b5d88288e9f12b6d5ba606cd
rajatkrkundu-aa <rajatkrkundu@gmail.com>
2026-07-26 17:23:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import streamlit as st
+from modules.app_menu import AppMenu
 from modules.config import AppConfig
+from modules.oi_screen import NiftyOiScreen
 from modules.screen_manager import NiftyDashboardScreen
 
 
 st.set_page_config(page_title="Nifty 50 - 5s Live Analyzer", layout="wide")
 
 config = AppConfig()
-screen = NiftyDashboardScreen(config)
+menu = AppMenu()
+
+selected_menu_key = menu.render()
+
+if selected_menu_key == "oi":
+	screen = NiftyOiScreen(config)
+else:
+	screen = NiftyDashboardScreen(config)
+
 screen.render()
